ba7f52606f5ad81f650b2521f6ed22911050e30d
davidcabezas <38328394+dacavi@users.noreply.github.com>
2021-01-21 18:56:27 -0500

    Esqueleto CRUD personas
    Se instalo nodemon para realizar el proceso mas agil y se creo el esqueleto del crud de personas

--- api.js
+++ api.js
@@ -1,5 +1,33 @@
-const express = reqire('express');
+const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+//CREATE personas
+app.post('/user/create', (req,res)=>{
+    console.log(req.body);
+    res.send(req.body);
+})
+
+// READ personas
+app.get('/user/read', (req,res)=>{
+    res.send('All users data');
+})
+
+// SiNGLE READ
+app.get('/user/single_read/:id', (req,res)=>{
+    res.send(`User ${req.params.id} data`)
+})
+
+//UPDATE personas
+app.put('/user/update/:id', (req,res)=>{
+    res.send(`User ${req.params.id} updated`)
+})
+
+//DELETE personas
+app.put('/user/delete/:id', (req,res)=>{
+    res.send(`User ${nombrec} deleted`)
+})
 
 app.listen(3000, () => {
     console.log('Server running at 3000');
